a01613033987f2081cdf425c9f89734801841c9e
Federico Bett <gfbett@gmail.com>
2020-11-29 21:03:52 -0300

    Implemented generic persistence

--- persistence.go
+++ persistence.go
@@ -1,25 +1,65 @@
 package main
 
 import (
-	//"fmt"
+	"io/ioutil"
+	"log"
 	"os"
 )
 
-func checkFolder(dir string) bool {
+type Writable interface {
+	toJson() string
+	loadFromJson(data []byte)
+	fileName() string
+}
+
+func CreateFolderIfNotExists(dir string) bool {
 	_, err := os.Stat(dir)
 	if err != nil && os.IsNotExist(err) {
-		err = os.Mkdir(dir, os.ModeDir)
+		err = os.Mkdir(dir, os.ModeDir|os.ModePerm)
 		if err == nil {
 			return true
+		} else {
+			return false
 		}
 	}
-	return false
+	return true
 }
 
-func (list *TaskList) Store() {
+func Store(writable Writable, path string) bool {
 
+	filename := writable.fileName()
+	data := writable.toJson()
+	file, err := os.Create(path + "/" + filename)
+	if err != nil {
+		return false
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	_, err = file.WriteString(data)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
-func (list *TaskList) Load() {
-
+func Load(writable Writable, path string) bool {
+	filename := writable.fileName()
+	file, err := os.Open(path + "/" + filename)
+	if err != nil {
+		return false
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	all, err := ioutil.ReadAll(file)
+	if err != nil {
+		return false
+	}
+	writable.loadFromJson(all)
+	return true
 }
--- persistence_test.go
+++ persistence_test.go
@@ -1,12 +1,60 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
+	"os"
 	"testing"
 )
 
-func Persistence_checkFolder(t *testing.T) {
+func TestPersistenceCheckFolder(t *testing.T) {
 
-	
+	testFolder := "testdata/wrongFolder"
+	_ = os.Remove(testFolder)
 
+	if !CreateFolderIfNotExists("testdata/existingFolder") {
+		t.Fail()
+	}
 
+	if !CreateFolderIfNotExists(testFolder) {
+		t.Fail()
+	}
+
+	_ = os.Remove(testFolder)
+}
+
+func TestStore(t *testing.T) {
+	test := new(TestWritable)
+	path := "testdata"
+	if !Store(test, path) {
+		t.Fail()
+	}
+	other := new(TestWritable)
+	if !Load(other, path) {
+		t.Fail()
+	}
+	if other.Test == false {
+		t.Error("Unexpected value")
+	}
+}
+
+type TestWritable struct {
+	Test bool
+}
+
+func (w *TestWritable) toJson() string {
+	return "{\"Test\":true}"
+}
+
+func (w *TestWritable) loadFromJson(data []byte) {
+	var loaded TestWritable
+	err := json.Unmarshal(data, &loaded)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Test = loaded.Test
+}
+
+func (w *TestWritable) fileName() string {
+	return "test.json"
 }
